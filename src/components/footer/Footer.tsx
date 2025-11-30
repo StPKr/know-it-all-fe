3ef507d1e72259a9c3fa85e6dac6044276043ceb
StPKr <sotfuni25@gmail.com>
2025-11-30 19:17:52 +0200

    polish

--- src/components/footer/Footer.tsx
+++
@@ -1,9 +0,0 @@
-export default function Footer() {
-    return (
-        <footer className="w-full bg-gray-900 text-gray-300 py-4 flex  justify-center">
-            <p className="text-sm">
-                &copy; {new Date().getFullYear()} My Company. All rights reserved.
-            </p>
-        </footer>
-    );
-}
--- src/components/header/Logo.tsx
+++
@@ -1,7 +0,0 @@
-export default function Logo() {
-  return (
-    <div className="w-8 h-8 bg-white rounded-full flex items-center justify-center text-blue-600 font-bold">
-      Q
-    </div>
-  );
-}
